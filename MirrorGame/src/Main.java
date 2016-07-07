04c6e2583413d1dbd9ae7a81c1a170aacfd4da9c
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-07 16:13:15 -0700

    Update Main.java

--- MirrorGame/src/Main.java
+++ MirrorGame/src/Main.java
@@ -1,7 +1,14 @@
-
-public static void main(String[] args){
-		
-		Display Display = new Display();
-	
-  
+/**
+ * @author Adriano Hernandez
+ * @version 0.11
+ * @date 7 July 2016
+ * 
+ * This is the main class where the game will be run.
+ * 
+ **/
+public class Main{
+	public static void main(String[] args){
+		Display gameWindow;
+		gamewindow = new Display(); //will automatically start it
+	}
 }
