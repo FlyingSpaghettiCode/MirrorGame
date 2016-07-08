833d64467368d1c195283f73e2c350d8211eafd4
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-08 12:37:32 -0700

    Update PlayerTreeNode.java

--- MirrorGame/src/players/PlayerTreeNode.java
+++ MirrorGame/src/players/PlayerTreeNode.java
@@ -1,3 +1,35 @@
-public class PlayerTreeNode{
+/**
+ * PlayerTreeNode handles calculations and dependencies on the player tree on a local scale
+ * between nodes.  Unlike the player class it does not deal with moving or display, just the abstract.
+ * 
+ * @author Adriano Hernandez
+ * @version 0.1
+ * @date 8 July 2016
+ * 
+ **/ 
+public abstract class PlayerTreeNode{
+  private Player player; //its player
   
+  private PlayerTreeNode parent; //the parent node
+  private PlayerTreeNode[] children; //children of this node (can have more than two)
+  
+  public PlayerTreeNode(){}
+  
+  //getters and basic methods
+  public PlayerTreeNode parent(){
+    return parent;
+  }
+  public Player player(){
+    return player;
+  }
+  public PlayerTreeNode[] children(){
+    return children;
+  }
+  public boolean isRoot(){
+    return parent == null;
+  }
+  
+  //translation function
+  public abstract double translateFunctionX(double input);
+  public abstract double translateFunctionY(double input);
 }
