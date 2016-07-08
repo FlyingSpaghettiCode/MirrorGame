2168c134eebceb2434263e6f4d3a34d69e462d38
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-08 14:42:52 -0700

    Update Player.java

--- MirrorGame/src/players/Player.java
+++ MirrorGame/src/players/Player.java
@@ -5,25 +5,7 @@
  * @version 0.1
  * @date 9 July 2016
  **/
-public abstract class Player{
-  //private PlayerTreeNode node; // this will be left up to children classes to decide
-  private double x; //x position at top left corner
-  private double y; //y position at top left corner
-  private double w; //width
-  private double h; //height
-  private WorldType type; //world or color we are at
-  
-  public Player(){}
-  
-  public double xPosition(){return x;}
-  public double yPosition(){return y;}
-  public double width(){return height;}
-  public double height(){return height;}
-  public WorldType type(){return type;}
-  
-  public double area(){return w*h;}
-  
+public abstract class Player extends GameObject{
   public void translateX(double x);
   public void translateY(double y);
-  public void display();
 }
