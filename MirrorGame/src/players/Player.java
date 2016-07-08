0c2a86b0cf280fd1a552053d45c3c35a45b6b89d
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-08 12:48:27 -0700

    Update Player.java

--- MirrorGame/src/players/Player.java
+++ MirrorGame/src/players/Player.java
@@ -6,7 +6,7 @@
  * @date 9 July 2016
  **/
 public abstract class Player{
-  private PlayerTreeNode node; //our position in the playerTreeNode
+  //private PlayerTreeNode node; // this will be left up to children classes to decide
   private double x; //x position at top left corner
   private double y; //y position at top left corner
   private double w; //width
@@ -14,6 +14,14 @@ public abstract class Player{
   private WorldType type; //world or color we are at
   
   public Player(){}
+  
+  public double xPosition(){return x;}
+  public double yPosition(){return y;}
+  public double width(){return height;}
+  public double height(){return height;}
+  public WorldType type(){return type;}
+  
+  public 
   
   public void translateX(double x);
   public void translateY(double y);
