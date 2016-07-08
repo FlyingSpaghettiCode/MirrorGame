de8ad3bd6e73ab074b1126894bc288161c6d5fff
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-08 12:39:33 -0700

    Update Player.java

--- MirrorGame/src/players/Player.java
+++ MirrorGame/src/players/Player.java
@@ -2,10 +2,11 @@
  * This is a simple player class.
  * 
  * @author Adriano Hernandez
- * @version .1
+ * @version 0.1
  * @date 9 July 2016
  **/
 public abstract class Player{
+  private PlayerTreeNode node; //our position in the playerTreeNode
   private double x; //x position at top left corner
   private double y; //y position at top left corner
   private double w; //width
@@ -13,7 +14,6 @@ public abstract class Player{
   private WorldType type; //world or color we are at
   
   public Player(){}
-  public Player(double x, double y){}
   
   public void translateX(double x);
   public void translateY(double y);
