bd251a36eebfd38adf08757a03536ea07fca31ad
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-08 17:35:31 -0700

    Update TerrainType.java

--- MirrorGame/src/TerrainType.java
+++ MirrorGame/src/TerrainType.java
@@ -1,4 +1,6 @@
 /** Types of terrain **/
 public enum TerrainType{
-  
+  SAFE,UNSAFE,IMPASSABLE
+  //white terrain is safe and black terrain is unsafe
+  //impassable terrain can not be passed on given worlds (colors)
 }
