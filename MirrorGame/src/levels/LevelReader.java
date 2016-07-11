98c95de39ca9b9fc827fb308df126ce786414408
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-10 17:26:47 -0700

    Update LevelReader.java

--- MirrorGame/src/levels/LevelReader.java
+++ MirrorGame/src/levels/LevelReader.java
@@ -1,5 +1,16 @@
 /** parse through the level files and make a level from that **/
 
 public class LevelReader{
-  private String[] lines;
+  
+  private Command[] commands;
+  
+  //parse our comamnds
+  public void parse(){
+    
+  }
+  private class Command{
+    String command;
+    String params;
+    
+  }
 }
