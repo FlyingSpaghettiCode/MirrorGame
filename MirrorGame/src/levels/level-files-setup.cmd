0a28d0d689de0a6cdd69844fc1584120d8de1c86
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-21 15:06:43 -0700

    Add files via upload

---
+++ MirrorGame/src/levels/level-files-setup.cmd
@@ -0,0 +1,7 @@
+:: Author 	Adriano Hernandez @ SpaghettiCodeGames
+:: Version 	0.1 alpha
+:: Date 	21 July 2016
+
+cd C:\Spaghetti\level-files
+@echo #this is a comment> "C:\Spaghetti\level-files\test-level.txt"
+@echo created test-level.txt
---
+++ MirrorGame/src/levels/resources-setup.cmd
@@ -0,0 +1,14 @@
+:: Author 	Adriano Hernandez @ SpaghettiCodeGames
+:: Version 	0.1 alpha
+:: Date 	21 July 2016
+::RUN THIS FROM C:\
+
+cd C:\
+call game-directories-setup
+cd C:\
+call level-files-setup
+
+@echo This should have:
+@echo 1. Created a directory structure with Spaghetti in C:\ and level-files and misc in Spaghetti.
+@echo 2. Created a test file in level-files.
+pause
