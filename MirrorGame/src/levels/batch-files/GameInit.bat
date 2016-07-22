c73970739deb3c5fbd50cebb0dfab82fbb3c9563
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-22 16:50:33 -0700

    Add files via upload

---
+++ MirrorGame/src/levels/batch-files/GameInit.bat
@@ -0,0 +1,13 @@
+::this is an improved init script
+::this can be run from anywhere
+
+::DIRECTORY
+mkdir C:\Spaghetti
+mkdir C:\Spaghetti\level-files
+mkdir C:\Spaghetti\sound-files
+mkdir C:\Spaghetti\global-settings
+mkdir C:\Spaghetti\game-files
+::TEXT
+@echo on
+@echo finished setting up basic directory structure for game files
+@echo FROM null > C:\Spaghetti\level-files\test-level.txt
