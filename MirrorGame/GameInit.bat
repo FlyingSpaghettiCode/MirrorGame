660136255e0e455f9e6db1c87750d8b0d97af2ae
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-23 00:11:16 -0700

    Update GameInit.bat

--- MirrorGame/GameInit.bat
+++ MirrorGame/GameInit.bat
@@ -14,9 +14,11 @@ mkdir C:\Spaghetti\game-files
 
 ::TEXT FOR TEST LEVEL
 @echo #### This is a test level file #### > C:\Spaghetti\level-files\test-level.txt
-@echo #### Author = Adriano Hernandez #### > C:\Spaghetti\level-files\test-level.txt
-@echo #### Version = 0.1 #### > C:\Spaghetti\level-files\test-level.txt
-@echo #### 23 7 2016 #### > C:\Spaghetti\level-files\test-level.txt
+::    Subsequent echoes rewrite the first line instead of adding new lines.
+::    It should be able to write line by line, or preferably I will just move files from our download.
+::@echo #### Author = Adriano Hernandez #### > C:\Spaghetti\level-files\test-level.txt
+::@echo #### Version = 0.1 #### > C:\Spaghetti\level-files\test-level.txt
+::@echo #### 23 7 2016 #### > C:\Spaghetti\level-files\test-level.txt
 
 ::PAUSE
 pause
