a6f6be9cc4bd62316cc0321d6ada10d93dfcb21d
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-25 22:50:08 -0700

    Add files via upload

--- GameInit.bat
+++ GameInit.bat
@@ -1,24 +1,7 @@
-::this is an improved init script
-::this can be run from anywhere
-
-::DIRECTORY SETUP
-mkdir C:\Spaghetti
-mkdir C:\Spaghetti\level-files
-mkdir C:\Spaghetti\sound-files
-mkdir C:\Spaghetti\global-settings
-mkdir C:\Spaghetti\game-files
-
-::PRINT
-@echo on
-@echo finished setting up basic directory structure for game files
-
-::TEXT FOR TEST LEVEL
-@echo #### This is a test level file #### > C:\Spaghetti\level-files\test-level.txt
-::    Subsequent echoes rewrite the first line instead of adding new lines.
-::    It should be able to write line by line, or preferably I will just move files from our download.
-::@echo #### Author = Adriano Hernandez #### > C:\Spaghetti\level-files\test-level.txt
-::@echo #### Version = 0.1 #### > C:\Spaghetti\level-files\test-level.txt
-::@echo #### 23 7 2016 #### > C:\Spaghetti\level-files\test-level.txt
-
-::PAUSE
+::most stuff will be in a jar, but level files and settings will be seperate
+@echo creating directories within your default documents folder
+mkdir C:\Users\%username%\Documents\MirrorGame
+mkdir C:\Users\%username%\Documents\MirrorGame\LevelFiles
+mkdir C:\Users\%username%\Documents\MirrorGame\Settings
+@echo Mirror Game level files and settings stored in txt documents here > C:\Users\%username%\Documents\MirrorGame\README.txt
 pause
