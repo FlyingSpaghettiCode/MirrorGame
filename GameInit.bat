d3d44ff378232898c68250addf1e0da74b588de0
18AdrianoH <adrianohernandez2000@gmail.com>
2016-07-31 12:05:37 -0700

    Update GameInit.bat

--- GameInit.bat
+++ GameInit.bat
@@ -1,7 +1,9 @@
 ::most stuff will be in a jar, but level files and settings will be seperate
 @echo creating directories within your default documents folder
-mkdir C:\Users\%username%\Documents\MirrorGame
-mkdir C:\Users\%username%\Documents\MirrorGame\LevelFiles
-mkdir C:\Users\%username%\Documents\MirrorGame\Settings
+cd C:\Users\%username%\Documents
+mkdir MirrorGame
+cd MirrorGame
+mkdir LevelFiles
+mkdir Settings
 @echo Mirror Game level files and settings stored in txt documents here > C:\Users\%username%\Documents\MirrorGame\README.txt
 pause
