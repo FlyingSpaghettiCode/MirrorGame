ffb013d1598008e8416ad10e958251a559dd72ce
18AdrianoH <adrianohernandez2000@gmail.com>
2016-08-01 06:42:33 -0700

    Add files via upload

--- MirrorGame/src/levels/LevelReader.java
+++ MirrorGame/src/levels/LevelReader.java
@@ -10,6 +10,8 @@ import javafx.scene.Scene;
 /**
  * Parses a text document that tells how to build the level.
  * It also can write them.
+ * 
+ * This is basicallly a factory class
  * @author Adriano
  *
  */
@@ -35,8 +37,9 @@ public class LevelReader {
 	@SuppressWarnings("unused")
 	public Level createLevel(Scene scene){
 		Level level;
+		List<String> lines;
 		try {
-			List<String> lines = docText();
+			lines = docText();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,14 +47,40 @@ public class LevelReader {
 		}
 		//level creation code from txt
 		level = new STDLevel(scene);
-		
+		simplify(lines);
 		
 		
 		//return
 		return level;
 	}
-	
-	
+	//return without any \ or && or anything like that
+	//(just break up into one command per line format for ease of parsing)
+	//won't break on null, but won't do anything
+	private void simplify(List<String> original){
+		if(original == null) return;
+		for(int i = original.size(); i > 0 ; i--){
+			if(original.get(i).substring(0, 1).equals("\\")){
+				original.set(i-1, original.get(i-1) + 
+						original.get(i).substring(1));
+				original.remove(i);
+			}
+		}
+		int firstAnd = 0;
+		while(contains(original,"&&")){
+			for(int i = firstAnd; i < original.size(); i++){
+				String ii = original.get(i);
+				int index = ii.indexOf("&&");
+				if(index != -1){
+					original.add(ii.substring(ii.indexOf("&&")+2));
+					original.set(i, ii.substring(0, ii.indexOf("&&")));
+				}
+			}
+		}
+	}
+	private boolean contains(List<String> ls, String s){
+		for(String str: ls) if(str.contains(s)) return true;
+		return false;
+	}
 	
 	//getters
 	public String getPATH() {
@@ -62,8 +91,12 @@ public class LevelReader {
 	}
 
 	
-	
-	@SuppressWarnings("unused")
+	/**
+	 * This is a standard level.  We can write other implementations of our
+	 * abstract level somewhere else.
+	 * @author Adriano Hernandez
+	 *
+	 */
 	private class STDLevel extends Level{
 
 		public STDLevel(Scene scene) {
