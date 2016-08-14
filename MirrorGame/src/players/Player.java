be531eb5ba7877023058e67dac81639f5df1f42d
edgar-lin-audacy <edgar.lin@audacy.space>
2016-08-14 16:41:38 -0700

    Changed to match MoveableSprite

--- MirrorGame/src/players/Player.java
+++ MirrorGame/src/players/Player.java
@@ -1,29 +1,28 @@
 package players;
 
-import levels.Level;
 import sprites.Collidable;
 import sprites.MoveableSprite;
 
 //for now just a controllable sprite that knows it's a collidable
 public class Player extends MoveableSprite implements Collidable{
 
-	public Player(Level level) {
-		super(level);
+	public Player() {
+		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Player(Level level, double x, double y) {
-		super(level, x, y);
+	public Player(double x, double y) {
+		super(x, y);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Player(Level level, double xPosition, double yPosition, String PATH) {
-		super(level, xPosition, yPosition, PATH);
+	public Player(double xPosition, double yPosition, String PATH) {
+		super(xPosition, yPosition, PATH);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Player(Level level, String PATH) {
-		super(level, PATH);
+	public Player(String PATH) {
+		super(PATH);
 		// TODO Auto-generated constructor stub
 	}
 
