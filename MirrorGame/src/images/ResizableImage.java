a27d7119675e4b814da599b696eebac3e96aec41
spagovir <edgarlin2000@gmail.com>
2016-11-27 13:25:26 -0800

    Edited drawing so that sprites rendered in the right color.

--- MirrorGame/src/images/ResizableImage.java
+++ MirrorGame/src/images/ResizableImage.java
@@ -7,11 +7,15 @@ public class ResizableImage extends Image {
 	
 	double height;
 	double width;
-
+        double xRatio;
+        double yRatio;
+        
 	public ResizableImage(String arg0) {
 		super(arg0);
 		height = this.getHeight();
 		width = this.getWidth();
+                xRatio = 1.;
+                yRatio = 1.;
 	}
 
 	// Gets the height and width that is actually used.
@@ -22,12 +26,22 @@ public class ResizableImage extends Image {
 	public double getActualWidth(){
 		return width;
 	}
+        public double getXRatio()
+        {
+            return xRatio;
+        }
+        public double getYRatio()
+        {
+            return yRatio;
+        }
 	
 	public void setHeight(double h){
 		height = h;
+                yRatio = getHeight()/height;
 	}
 	
 	public void setWidth(double w){
 		width = w;
+                xRatio = getWidth()/width;
 	}
 }
